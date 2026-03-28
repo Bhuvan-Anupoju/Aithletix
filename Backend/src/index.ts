74ca62c47e77e8cac6b570ca7a5b29972c3a157b
Bhuvan A <abhuvan1214@gmail.com>
2026-03-28 21:12:06 +0530

    updated cors

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -10,7 +10,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://aithletix-bhuvan.onrender.com",
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 app.use(express.json());
 
